b700e905f876d4815f673fa3c752aa575705a4f7
Kerwin Mayorga <kmayor1@live.spcollege.com>
2022-01-07 00:50:38 -0500

    second commit

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+    # pk_f012f44498454daeabdf70e2c2a2edac
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_f012f44498454daeabdf70e2c2a2edac")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
